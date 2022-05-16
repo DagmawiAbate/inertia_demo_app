26853dccd0fce27db8a49db2cdcf2cf486b17bd4
DagmawiAbate <dagmawiabate2@gmail.com>
2022-05-16 11:10:08 +0300

    Initial commit

---
+++ resources/js/Components/Table.vue
@@ -0,0 +1,13 @@
+<template>
+  <table class="w-full whitespace-nowrap">
+    <thead>
+      <tr class="font-bold text-left">
+        <slot name="header">
+        </slot>
+      </tr>
+    </thead>
+    <tbody>
+      <slot/>
+    </tbody>
+  </table>
+</template>
---
+++ resources/js/Pages/Another.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <p>{{ $page.props.appName }}</p>
+    <inertia-link :href=" route('home') ">Home</inertia-link>
+    This is Another Page.
+  </div>
+</template>
